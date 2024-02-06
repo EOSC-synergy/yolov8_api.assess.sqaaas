6fc511f2824ba95c6be3f931bd85ea98cbacedb0
orviz <orviz@ifca.unican.es>
2024-02-06 14:50:09 +0100

    Add JePL file structure

---
+++ .sqa/script.cloudy-maroon-octopus.sh
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd codebase.helmholtz.cloud/m-team/ai/yolov8_api &&
+    get_git_tags.py
+)
---
+++ .sqa/script.ugly-denim-crocodile.sh
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd codebase.helmholtz.cloud/m-team/ai/yolov8_api &&
+    find_doc_files.py --file_type all
+)
